ba1a7062b86bc4fabc7a5aeaebc81f0ecc23c289
Rahix <rahix@rahix.de>
2018-09-18 22:07:40 +0200

    Initial
    Signed-off-by: Rahix <rahix@rahix.de>

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![feature(lang_items, unwind_attributes)]
+
+
+extern crate atmega32u4;
+extern crate atmega32u4_hal;
+
+use atmega32u4_hal::{delay, port};
+
+pub type Delay = delay::Delay<delay::MHz16>;
+
+pub type LedBuiltin = port::portc::PC7<port::Output>;
+pub type LedBuiltinRX = port::portb::PB0<port::Output>;
+pub type LedBuiltinTX = port::portd::PD5<port::Output>;
+
+pub mod std {
+    use super::atmega32u4;
+
+    #[lang = "eh_personality"]
+    #[no_mangle]
+    pub unsafe extern "C" fn rust_eh_personality(_state: (), _exception_object: *mut (), _context: *mut ()) -> () {
+    }
+
+    #[lang = "panic_fmt"]
+    #[unwind]
+    #[no_mangle]
+    pub unsafe extern "C" fn rust_begin_panic(_msg: (), _file: &'static str, _line: u32) -> ! {
+        use atmega32u4_hal::prelude::*;
+
+        let mut delay = super::Delay::new();
+
+        let mut portb = ::core::mem::uninitialized::<atmega32u4::PORTB>().split();
+        let mut portd = ::core::mem::uninitialized::<atmega32u4::PORTD>().split();
+
+        let mut led0 = portb.pb0.into_output(&mut portb.ddr);
+        let mut led1 = portd.pd5.into_output(&mut portd.ddr);
+
+        loop {
+            led0.set_high();
+            delay.delay_ms(100);
+            led0.set_low();
+
+            led1.set_high();
+            delay.delay_ms(100);
+            led1.set_low();
+        }
+    }
+}
